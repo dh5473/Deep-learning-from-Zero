3f4f48859fe54c2d9c7062364ebfafa48fa16048
dh5473 <dh5473@naver.com>
2023-01-09 00:10:08 +0900

    step08

---
+++ steps/step08.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+    
+    def backward(self):
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()  # 함수 가져오기
+            x, y = f.input, f.output  # 함수의 입력, 출력 가져오기
+            x.grad = f.backward(y.grad)  # backward 메서드 호출
+
+            if x.creator is not None:
+                funcs.append(x.creator)  # 하나 앞의 함수 리스트에 추가
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        output.set_creator(self)
+        self.input = input
+        self.output = output
+        return output
+    
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+# 역전파
+y.grad = np.array(1.0)
+y.backward()
+print(x.grad)  # 3.297442541400256
