080cafed9b89a6bc840326fca085766953004d0f
dh5473 <dh5473@naver.com>
2023-02-13 11:58:26 +0900

    Feat reshape, transpose function

--- dezero/functions.py
+++ dezero/functions.py
@@ -1,7 +1,10 @@
 import numpy as np
-from dezero.core import Function
+from dezero.core import Function, Variable, as_variable, as_array
 
 
+# =============================================================================
+# Basic functions: sin / cos / tanh / exp / log
+# =============================================================================
 class Sin(Function):
     def forward(self, x):
         y = np.sin(x)
@@ -45,3 +48,46 @@ class Tanh(Function):
 
 def tanh(x):
     return Tanh()(x)
+
+
+# =============================================================================
+# Tensor operations: reshape / transpose / get_item / expand_dims / flatten
+# =============================================================================
+class Reshape(Function):
+    def __init__(self, shape):
+        self.shape = shape
+
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = x.reshape(self.shape)
+        return y
+    
+    def backward(self, gy):
+        return reshape(gy, self.x_shape)
+
+
+def reshape(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return Reshape(shape)(x)
+
+
+class Transpose(Function):
+    def __init__(self, axes=None):
+        self.axes = axes
+
+    def forward(self, x):
+        y = x.transpose(self.axes)
+        return y
+    
+    def backward(self, gy):
+        if self.axes is None:
+            return transpose(gy)
+        
+        axes_len = len(self.axes)
+        inv_axes = tuple(np.argsort([ax % axes_len for ax in self.axes]))
+        return transpose(gy, inv_axes)
+
+
+def transpose(x, axes=None):
+    return Transpose(axes)(x)
