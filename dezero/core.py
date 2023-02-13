6663f5685bc0f3d9b58d7bd54dfe305bea8530b7
dh5473 <dh5473@naver.com>
2023-02-13 11:57:44 +0900

    Feat reshape, transpose function

--- dezero/core.py
+++ dezero/core.py
@@ -1,6 +1,7 @@
 import weakref
 import numpy as np
 import contextlib
+import dezero
 
 
 # =============================================================================
@@ -109,6 +110,23 @@ class Variable:
             if not retain_grad:
                 for y in f.outputs:
                     y().grad = None  # y is weakref
+    
+    def reshape(self, *shape):
+        if len(shape) == 1 and isinstance(shape[0], (tuple, list)):
+            shape = shape[0]
+        return dezero.functions.reshape(self, shape)
+    
+    def transpose(self, *axes):
+        if len(axes) == 0:
+            axes = None
+        elif len(axes) == 1:
+            if isinstance(axes[0], (tuple, list)) or axes[0] is None:
+                axes = axes[0]
+        return dezero.functions.transpose(self, axes)
+    
+    @property
+    def T(self):
+        return dezero.functions.transpose(self)
 
 
 def as_variable(obj):
