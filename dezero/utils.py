6359ce87ea24e30559c5866fc4cc93bd08b2d365
dh5473 <dh5473@naver.com>
2023-02-15 00:51:05 +0900

    Feat sum_to function

--- dezero/utils.py
+++ dezero/utils.py
@@ -83,8 +83,24 @@ def plot_dot_graph(output, verbose=True, to_file='graph.png'):
 # =============================================================================
 # Utility functions for numpy (numpy magic)
 # =============================================================================
+def sum_to(x, shape):
+    """
+    Sum elements along axes to output an array of a given shape.
+    """
+    ndim = len(shape)
+    lead = x.ndim - ndim
+    lead_axis = tuple(range(lead))
+
+    axis = tuple([i + lead for i, sx in enumerate(shape) if sx == 1])
+    y = x.sum(lead_axis + axis, keepdims=True)
+    if lead > 0:
+        y = y.squeeze(lead_axis)
+    return y
+
+
 def reshape_sum_backward(gy, x_shape, axis, keepdims):
-    """Reshape gradient appropriately for dezero.functions.sum's backward.
+    """
+    Reshape gradient appropriately for dezero.functions.sum's backward.
     """
     ndim = len(x_shape)
     tupled_axis = axis
