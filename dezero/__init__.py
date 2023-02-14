d32c0e042d345f90c08b44422307c95b8bb59f3c
dh5473 <dh5473@naver.com>
2023-02-15 00:48:02 +0900

    Feat import module

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -16,6 +16,9 @@ else:
     from dezero.core import as_array
     from dezero.core import as_variable
     from dezero.core import setup_variable
-    
+    from dezero.core import Config
+
+    import dezero.functions
+    import dezero.utils
 
 setup_variable()
