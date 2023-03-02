5b2099de5a43c4595d05d5ebffa9c680359199fc
dh5473 <dh5473@naver.com>
2023-03-02 19:03:14 +0900

    Feat accuracy function

--- dezero/functions.py
+++ dezero/functions.py
@@ -387,6 +387,13 @@ def softmax_cross_entropy_simple(x, t):
 # =============================================================================
 # accuracy / dropout / batch_norm / embed_id
 # =============================================================================
+def accuracy(y, t):
+    y, t = as_variable(y), as_variable(t)
+
+    pred = y.data.argmax(axis=1).reshape(t.shape)
+    result = (pred == t.data)
+    acc = result.mean()
+    return Variable(as_array(acc))
 
 
 # =============================================================================
